59ed390431f2f1c98480b19048d1980f779298a4
himanshu17-2000 <64321714+himanshu17-2000@users.noreply.github.com>
2022-01-08 21:14:47 +0530

    all good

--- src/service/api.js
+++ src/service/api.js
@@ -1,5 +1,6 @@
 import axios from "axios"
-const url = "http://localhost:8000"
+// const url = "http://localhost:8000"
+const url ="https://earthboxerserver.herokuapp.com"
 export const createPost = async (post) => {
     try {
         await axios.post(`${url}/create`, post)
